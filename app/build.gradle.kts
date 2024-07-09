35b6c2a47e7b974e268ceafbd71f166a16050b56
FelipeOsorio-Android <feeh_devandroid@hotmail.com>
2024-07-09 00:02:19 -0300

    add lib navigation and att lib kotlin 2.0.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
+    alias(libs.plugins.compose.compiler)
+    kotlin("plugin.serialization") version "2.0.0"
 }
 
 android {
@@ -56,6 +58,12 @@ dependencies {
 
     //Lottie
     implementation(libs.lottie.compose)
+
+    //Navigation
+    implementation(libs.androidx.navigation.compose)
+
+    //Serialization
+    implementation(libs.kotlinx.serialization.json)
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
