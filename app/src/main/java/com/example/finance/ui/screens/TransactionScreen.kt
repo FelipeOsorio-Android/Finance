5d70c30c732fa95716409811f187f787ecd7724d
FelipeOsorio-Android <feeh_devandroid@hotmail.com>
2024-07-09 00:05:17 -0300

    create screens app

---
+++ app/src/main/java/com/example/finance/ui/screens/TransactionScreen.kt
@@ -0,0 +1,30 @@
+package com.example.finance.ui.screens
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.finance.ui.theme.FinanceTheme
+
+@Composable
+fun TransactionScreen(modifier: Modifier = Modifier) {
+    Column(
+        Modifier
+            .fillMaxSize()
+            .background(color = Color.Blue)
+    ) {
+
+    }
+}
+
+
+@Preview
+@Composable
+private fun TransactionScreenPreview() {
+    FinanceTheme {
+        TransactionScreen()
+    }
+}
