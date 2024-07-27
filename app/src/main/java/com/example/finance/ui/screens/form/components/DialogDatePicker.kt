a88af63f0f2beabfbf6c9298dd088e54df954007
FelipeOsorio-Android <feeh_devandroid@hotmail.com>
2024-07-27 16:27:50 -0300

    refactoring logic

--- app/src/main/java/com/example/finance/ui/screens/form/components/DialogDatePicker.kt
+++ app/src/main/java/com/example/finance/ui/screens/form/components/DialogDatePicker.kt
@@ -8,6 +8,9 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.material3.rememberDatePickerState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.finance.ui.theme.FinanceTheme
@@ -16,19 +19,33 @@ import com.example.finance.ui.theme.FinanceTheme
 @Composable
 fun DialogDatePicker(
     modifier: Modifier = Modifier,
+    showDatePicker: Boolean = false,
     state: DatePickerState = rememberDatePickerState(),
     onDataSelected: (Long?) -> Unit = {},
     onDismissRequest: () -> Unit = {},
 ) {
-    DatePickerDialog(
-        onDismissRequest = onDismissRequest,
-        confirmButton = { 
-            TextButton(onClick = { onDataSelected(state.selectedDateMillis) }) {
-                Text(text = "Confirmar")
-            }
+    val isBtnEnabled by remember { derivedStateOf{ state.selectedDateMillis != null } }
+
+    if (showDatePicker) {
+        DatePickerDialog(
+            onDismissRequest = onDismissRequest,
+            confirmButton = {
+                TextButton(
+                    onClick = { onDataSelected(state.selectedDateMillis) },
+                    enabled = isBtnEnabled
+                ) {
+                    Text(text = "Confirmar")
+                }
+            },
+            dismissButton = {
+                TextButton(onClick = onDismissRequest) {
+                    Text(text = "Cancelar")
+                }
+            },
+            modifier = modifier
+        ) {
+            DatePicker(state = state)
         }
-    ) {
-        DatePicker(state = state)
     }
 }
 
