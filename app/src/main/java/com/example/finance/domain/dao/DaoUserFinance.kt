5beaacc3534f7fd169105c6f685c7a9c5234b99a
FelipeOsorio-Android <feeh_devandroid@hotmail.com>
2024-08-03 00:03:55 -0300

    add fun update

--- app/src/main/java/com/example/finance/domain/dao/DaoUserFinance.kt
+++ app/src/main/java/com/example/finance/domain/dao/DaoUserFinance.kt
@@ -4,6 +4,7 @@ import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
+import androidx.room.Update
 import com.example.finance.domain.entities.EntityUserFinance
 import kotlinx.coroutines.flow.Flow
 
@@ -13,6 +14,13 @@ interface DaoUserFinance {
     @Query(value = "SELECT * FROM EntityUserFinance")
     fun getAllFinance(): Flow<List<EntityUserFinance>>
 
+    @Query(value = "SELECT * FROM EntityUserFinance WHERE id = :id")
+    fun getUserFinance(id: String): Flow<EntityUserFinance?>
+
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun saveUserFinance(entityUserFinance: EntityUserFinance)
+
+    @Update(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun updateUserFinance(entityUserFinance: EntityUserFinance)
+
 }
